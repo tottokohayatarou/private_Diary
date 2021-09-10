2968ff94b9908de8e01ed6f06f040f56d8f59d4d
HAYATA KIMIZUKA <yongtaijunzhong@gmail.com>
2021-09-10 12:29:23 +0900

    ログイン、ログアウト

--- config/settings.py
+++ config/settings.py
@@ -40,11 +40,10 @@ INSTALLED_APPS = [
 
     'diary.apps.DiaryConfig',
     'accounts.apps.AccountsConfig',
-    # http://127.0.0.1:8000/admin/login/?next=/admin/
 
-    # 'django.contrib.sites',
-    # 'allauth',
-    # 'allauth.account',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
 
 ]
 
@@ -181,15 +180,24 @@ MESSAGE_TAGS = {
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # 216p
-# SITE_ID=1
+SITE_ID=1
 
-# AUTHENTICATION_BACKENDS=(
-#     'allauth.account.auth_backends.AuthentionBackend',
-#     'django.contrib.auth.backends.ModelBackend',
-# )
+AUTHENTICATION_BACKENDS=(
+    'allauth.account.auth_backends.AuthenticationBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
 
-# ACCOUNT_AUTHENTICATION_METHOD='email'
-# ACCOUNT_USERNAME_REQUIRED =False
+ACCOUNT_AUTHENTICATION_METHOD='email'
+ACCOUNT_USERNAME_REQUIRED =False
 
-# ACCOUNT_EMAIL_VERIFICATIONTION ='mandatory'
-# ACCOUNT_LOGOUT_REDIRECT_URL = True
+ACCOUNT_EMAIL_VERIFICATIONTION ='mandatory'
+ACCOUNT_EMAIL_REQUIRED = True
+
+LOGIN_REDIRECT_URL ='diary:index'
+ACCOUNT_LOGOUT_REDIRECT_URL = 'account_login'
+
+ACCOUNT_LOGOUT_ON_GET = True
+
+ACCOUNT_EMAIL_SUBJECT_PREFIX = ''
+
+DEFAULT_FROM_EMAIL = 'admin@example.com'
